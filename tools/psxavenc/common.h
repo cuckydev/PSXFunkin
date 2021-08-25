d915d477d37a048e4eccd7c731016f9fe0804b17
CuckyDev <regan_green27@hotmail.com>
2021-08-25 15:05:13 -0400

    Japi XAs

--- tools/psxavenc/common.h
+++ tools/psxavenc/common.h
@@ -19,6 +19,8 @@ freely, subject to the following restrictions:
 2. Altered source versions must be plainly marked as such, and must not be
    misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
+
+This tool has been modified so that libpsxav isn't an external library
 */
 
 #include <assert.h>
