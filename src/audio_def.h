bc5bbbbd6ebdd4990b85fa67e0edd678b27ccd4d
CuckyDev <regan_green27@hotmail.com>
2021-08-26 12:48:49 -0400

    add mp3 list

--- src/audio_def.h
+++ src/audio_def.h
@@ -36,7 +36,7 @@ static const XA_TrackDef xa_tracks[] = {
 	{XA_Week4B, XA_LENGTH(10300)}, //XA_Test
 	//WEEK5A.XA
 	{XA_Week5A, XA_LENGTH(11520)}, //XA_Cocoa
-	{XA_Week5A, XA_LENGTH(9401)}, //XA_Eggnog
+	{XA_Week5A, XA_LENGTH(9401)},  //XA_Eggnog
 	//WEEK5B.XA
 	{XA_Week5B, XA_LENGTH(13223)}, //XA_WinterHorrorland
 	//WEEK6A.XA
@@ -84,4 +84,63 @@ static const char *xa_paths[] = {
 	"\\MUSIC\\CLWNA.XA;1",  //XA_ClwnA
 	"\\MUSIC\\CLWNB.XA;1",  //XA_ClwnB
 	NULL,
+};
+
+static const struct
+{
+	const char *name;
+	boolean vocal;
+} mp3s[] = {
+	//MENU.XA
+	{"freaky", false},   //XA_GettinFreaky
+	{"gameover", false}, //XA_GameOver
+	//WEEK1A.XA
+	{"bopeebo", true}, //XA_Bopeebo
+	{"fresh", true},   //XA_Fresh
+	//WEEK1B.XA
+	{"dadbattle", true}, //XA_Dadbattle
+	{"tutorial", false}, //XA_Tutorial
+	//WEEK2A.XA
+	{"spookeez", true}, //XA_Spookeez
+	{"south", true},    //XA_South
+	//WEEK2B.XA
+	{"monster", true}, //XA_Monster
+	//WEEK3A.XA
+	{"pico", true},   //XA_Pico
+	{"philly", true}, //XA_Philly
+	//WEEK3B.XA
+	{"blammed", true}, //XA_Blammed
+	//WEEK4A.XA
+	{"satinpanties", true}, //XA_SatinPanties
+	{"high", true},         //XA_High
+	//WEEK4B.XA
+	{"milf", true}, //XA_MILF
+	{"test", true}, //XA_Test
+	//WEEK5A.XA
+	{"cocoa", true},  //XA_Cocoa
+	{"eggnog", true}, //XA_Eggnog
+	//WEEK5B.XA
+	{"winterhorrorland", true}, //XA_WinterHorrorland
+	//WEEK6A.XA
+	{"senpai", true}, //XA_Senpai
+	{"roses", true},  //XA_Roses
+	//WEEK6B.XA
+	{"thorns", true}, //XA_Thorns
+	//WEEK7A.XA
+	{"ugh", true},  //XA_Ugh
+	{"guns", true}, //XA_Guns
+	//WEEK7B.XA
+	{"stress", true}, //XA_Stress
+	//KAPIA.XA
+	{"wocky", true},     //XA_Wocky
+	{"beathoven", true}, //XA_Beathoven
+	//KAPIB.XA
+	{"hairball", true}, //XA_Hairball
+	{"nyaw", true},     //XA_Nyaw
+	//CLWNA.XA
+	{"improbableoutset", true}, //XA_ImprobableOutset
+	{"madness", true}, //XA_Madness
+	//CLWNB.XA
+	{"hellclown", true}, //XA_Hellclown
+	{"expurgation", true}, //XA_Expurgation
 };
