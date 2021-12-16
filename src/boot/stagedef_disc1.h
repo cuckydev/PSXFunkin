132e85d9c23f791e0c75e3bb874eb44a438f7678
cuckydev <regan_green27@hotmail.com>
2021-12-16 12:21:12 -0500

    week 2 3 4 mus

--- src/boot/stagedef_disc1.h
+++ src/boot/stagedef_disc1.h
@@ -24,44 +24,44 @@ static const StageDef stage_defs[StageId_Max] = {
 	
 	{ //StageId_2_1 (Spookeez)
 		"\\WEEK2\\WEEK2.EXE;1", Week2_SetPtr,
-		"\\WEEK1\\WEEK2_1.MUS;1"
+		"\\WEEK2\\WEEK2_1.MUS;1"
 	},
 	{ //StageId_2_2 (South)
 		"\\WEEK2\\WEEK2.EXE;1", Week2_SetPtr,
-		"\\WEEK1\\WEEK2_2.MUS;1"
+		"\\WEEK2\\WEEK2_2.MUS;1"
 	},
 	{ //StageId_2_3 (Monster)
 		"\\WEEK2\\WEEK2.EXE;1", Week2_SetPtr,
-		"\\WEEK1\\WEEK2_3.MUS;1"
+		"\\WEEK2\\WEEK2_3.MUS;1"
 	},
 	
 	{ //StageId_3_1 (Pico)
 		"\\WEEK3\\WEEK3.EXE;1", Week3_SetPtr,
-		"\\WEEK1\\WEEK3_1.MUS;1"
+		"\\WEEK3\\WEEK3_1.MUS;1"
 	},
 	{ //StageId_3_2 (Philly Nice)
 		"\\WEEK3\\WEEK3.EXE;1", Week3_SetPtr,
-		"\\WEEK1\\WEEK3_2.MUS;1"
+		"\\WEEK3\\WEEK3_2.MUS;1"
 	},
 	{ //StageId_3_3 (Blammed)
 		"\\WEEK3\\WEEK3.EXE;1", Week3_SetPtr,
-		"\\WEEK1\\WEEK3_3.MUS;1"
+		"\\WEEK3\\WEEK3_3.MUS;1"
 	},
 	
 	{ //StageId_4_1 (Satin Panties)
 		"\\WEEK4\\WEEK4.EXE;1", Week4_SetPtr,
-		"\\WEEK1\\WEEK4_1.MUS;1"
+		"\\WEEK4\\WEEK4_1.MUS;1"
 	},
 	{ //StageId_4_2 (High)
 		"\\WEEK4\\WEEK4.EXE;1", Week4_SetPtr,
-		"\\WEEK1\\WEEK4_2.MUS;1"
+		"\\WEEK4\\WEEK4_2.MUS;1"
 	},
 	{ //StageId_4_3 (MILF)
 		"\\WEEK4\\WEEK4.EXE;1", Week4_SetPtr,
-		"\\WEEK1\\WEEK4_3.MUS;1"
+		"\\WEEK4\\WEEK4_3.MUS;1"
 	},
 	{ //StageId_4_4 (Test)
 		"\\WEEK4\\WEEK4.EXE;1", Week4_SetPtr,
-		"\\WEEK1\\WEEK4_4.MUS;1"
+		"\\WEEK4\\WEEK4_4.MUS;1"
 	},
 };
